dbfcb14e77c57ccaf1d9a4bae2f579e49aa2331b
sik2 <bickwang123@gmail.com>
2024-08-20 09:58:15 +0900

    4단계

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -7,18 +7,20 @@ public class Main {
 
         System.out.println("== 게시판 앱 ==");
 
+        int lastId = 1;
         while (true) {
             System.out.print("명령) ");
-            String command = sc.nextLine();
+            String command = sc.nextLine().trim();
 
             if (command.equals("종료")) {
                 break;
             } else if (command.equals("등록")) {
                 System.out.print("제목 : ");
-                String subject = sc.nextLine();
+                String subject = sc.nextLine().trim();
                 System.out.print("내용 : ");
-                String content = sc.nextLine();
-                System.out.println("1번 게시물이 등록되었습니다.");
+                String content = sc.nextLine().trim();
+                System.out.printf("%d번 게시물이 등록되었습니다.\n", lastId);
+                lastId++;
             }
         }
         sc.close();
