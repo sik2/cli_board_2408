3acc41b223e4255f8ece4b2c51bfb52840ada82f
sik2 <bickwang123@gmail.com>
2024-08-20 10:20:30 +0900

    5단계 목록 출력

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -1,3 +1,5 @@
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
 
 public class App {
@@ -9,6 +11,7 @@ public class App {
 
     public void run () {
         int lastId = 1;
+        List<Article> articleList = new ArrayList<>();
 
         System.out.println("== 게시판 앱 ==");
         while (true) {
@@ -23,7 +26,19 @@ public class App {
                 System.out.print("내용 : ");
                 String content = sc.nextLine().trim();
                 System.out.printf("%d번 게시물이 등록되었습니다.\n", lastId);
+
+                Article article = new Article(lastId, subject, content);
+                articleList.add(article);
+
                 lastId++;
+            } else if (command.equals("목록")) {
+                System.out.println("번호 / 제목 / 내용");
+                System.out.println("----------------------");
+                for (int i = 0; i < articleList.size(); i++) {
+                    Article article = articleList.get(i);
+                    System.out.printf("%d / %s / %s\n", article.getId(), article.getSubject(), article.getContent());
+                }
+
             }
         }
     }
---
+++ src/main/java/Article.java
@@ -0,0 +1,23 @@
+public class Article {
+    private int id;
+    private String subject;
+    private String content;
+
+    Article (int id, String subject, String content) {
+        this.id = id;
+        this.subject = subject;
+        this.content = content;
+    }
+
+    int getId () {
+        return this.id;
+    }
+
+    String getSubject () {
+        return this.subject;
+    }
+
+    String getContent() {
+        return this.content;
+    }
+}
