4ac3c9473fef8d120f189869fe70e2b35d2c84bd
sik2 <bickwang123@gmail.com>
2024-08-20 09:39:30 +0900

    1단계 완

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -2,5 +2,19 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("== 게시판 앱 ==");
+
+        while (true) {
+            System.out.print("명령) ");
+            String command = sc.nextLine();
+
+            if (command.equals("종료")) {
+                break;
+            }
+        }
+        sc.close();
     }
 }
