a26b8777616bd9c8080eb1f8ddffcea33b3cde04
sik2 <bickwang123@gmail.com>
2024-08-20 10:06:57 +0900

    4단계 해결 후 구조 변경

---
+++ src/main/java/App.java
@@ -0,0 +1,30 @@
+import java.util.Scanner;
+
+public class App {
+    Scanner sc;
+
+    App (Scanner sc) {
+        this.sc = sc;
+    }
+
+    public void run () {
+        int lastId = 1;
+
+        System.out.println("== 게시판 앱 ==");
+        while (true) {
+            System.out.print("명령) ");
+            String command = sc.nextLine().trim();
+
+            if (command.equals("종료")) {
+                break;
+            } else if (command.equals("등록")) {
+                System.out.print("제목 : ");
+                String subject = sc.nextLine().trim();
+                System.out.print("내용 : ");
+                String content = sc.nextLine().trim();
+                System.out.printf("%d번 게시물이 등록되었습니다.\n", lastId);
+                lastId++;
+            }
+        }
+    }
+}
